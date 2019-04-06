a9604e92f37f1bf44a078c600a24927471084e7e
thg090020 <temesxgn@gmail.com>
2019-04-06 17:41:29 -0500

    fix prod script

--- scripts/start-prod.sh
+++ scripts/start-prod.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-echo ============= Building Production API =============
-docker build -f ../Dockerfile -t redeam/book-api ..
-
 echo ============= Composing Production API =============
 docker-compose -f ../docker-compose.yml up -d
 
